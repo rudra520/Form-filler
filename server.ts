21738d707507beec51877e1216cc501a8be1660a
Royalking0016 <rudra222000@gmail.com>
2026-09-22 22:05:26 +0530

    new adding

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   // JSON middleware
   app.use(express.json({ limit: '10mb' }));
